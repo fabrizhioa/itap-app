6174e252624e4608cd480b313cb26d5e21c0ed37
LouMogollonDev <91619676+luisomogollon@users.noreply.github.com>
2022-10-01 19:39:28 -0400

    cambios

--- src/components/Content2.jsx
+++ src/components/Content2.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Content2 = () => {
   return (
-    <section className="flex items-center justify-start py-10 text-white bg-Hero-new bg-cover bg-fixed bg-white sm:py-16 md:py-24 lg:py-32">
+    <section className="flex items-center justify-start py-10 text-white bg-Hero-new  bg-cover bg-fixed bg-white sm:py-16 md:py-24 lg:py-32">
       <div className="relative max-w-3xl px-10 text-center text-white auto lg:px-0">
         <div className="flex flex-col w-full md:flex-row">
           <div className="max-w-6xl px-4  mx-auto border-solid justify-start lg:px-12">
@@ -12,7 +12,7 @@ const Content2 = () => {
           </div>
         </div>
 
-        <div className="my-24  p-1 lg:my-52"></div>
+        <div className="my-32  p-1 lg:my-56 "></div>
       </div>
     </section>
   );
