87a44bd5f5ff2af12c706076789fa7f372880acb
LouMogollonDev <91619676+luisomogollon@users.noreply.github.com>
2022-10-03 16:38:06 -0400

    cambio

--- src/components/Content2.jsx
+++ src/components/Content2.jsx
@@ -4,34 +4,35 @@ import { BsShieldCheck } from "react-icons/bs";
 import { VscDeviceMobile } from "react-icons/vsc";
 
 const rowClassName =
-  "flex-auto w-full  border tr px-20 py-52 font-sans  bg-black bg-opacity-50 to-transparent ";
+  "flex-auto w-full border tr px-20 py-48 font-sans  bg-black bg-opacity-50 to-transparent ";
 
 const Content2 = () => {
   return (
-    <section className="text-white bg-Hero-new bg-cover bg-fixed">
+    <section className="text-white bg-Hero-new  bg-cover bg-fixed">
       <div className="w-full  flex  ">
         <div className={rowClassName}>
-          <svg className="w-14 h-14    fill-current" viewBox="0 0 15 16">
+          <svg className="w-14 h-14 mt-32 fill-current" viewBox="0 0 15 16">
             <AiOutlineDropbox />
           </svg>
-          <h1 className="text-lg font-bold tracking-wider  ">Recibe Listo </h1>{" "}
-          <h2 className="text-lg font-bold tracking-wider "> para Usar </h2>{" "}
+          <h1 className="text-lg font-bold tracking-wider ">Recibe Listo </h1>{" "}
+          <h2 className="text-lg font-bold tracking-wider"> para Usar </h2>{" "}
           <p className="text-sm mt-2  text-gray-300 ">
             Recibe todo listo para usar desde el primer día
           </p>
         </div>
         <div className={rowClassName}>
-          <svg className="w-14 h-14  fill-current" viewBox="0 0 15 16">
+        
+          <svg className="w-14 h-14   fill-current " viewBox="0 0 15 16">
             <AiOutlineWifi />
           </svg>
-          <h1 className="text-lg font-semibold tracking-wider">Tecnología</h1>{" "}
+          <h1 className="text-lg font-semibold tracking-wider ">Tecnología</h1>{" "}
           <h2 className="text-lg font-bold tracking-wider  "> NFC </h2>{" "}
           <p className="text-sm mt-2 text-gray-300 whitespace-pre-line tracking-wider  ">
             Nuestra tecnología es la manera mas avanzada
           </p>
         </div>
         <div className={rowClassName}>
-          <svg className="w-14 h-14  fill-current" viewBox="0 0 15 16">
+          <svg className="w-14 h-14 mt-32 fill-current" viewBox="0 0 15 16">
             <BsShieldCheck />
           </svg>
           <h1 className="text-lg font-semibold tracking-wider">Rapido</h1>{" "}
