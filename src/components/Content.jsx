9d0ffa229bc391643e7b859ad337092e39ae7817
LouMogollonDev <91619676+luisomogollon@users.noreply.github.com>
2022-10-04 18:42:03 -0400

    ++

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,18 +3,18 @@ import React from "react";
 
 const Content = () => {
   return (
-    <section className="text-white bg-hero-max  bg-cover bg-center  ">
-      <div class="max-w-6xl mx-auto py-1 lg:py-10 lg:px-14  ">
-        <p class="mt-2 text-4xl font-semibold text-start px-4 lg:px-20   text-gray-50  sm:tracking-tight lg:text-6xl">
+    <section className="text-white bg-hero-max  bg-cover bg-center lg:bg-center  ">
+      <div class="max-w-6xl  py-1 lg:py-10 lg:px-14  ">
+        <p class="mt-2 text-4xl font-semibold text-end lg:text-start px-4 lg:px-20   text-gray-50  sm:tracking-tight lg:text-6xl">
           Una Nueva
         </p>
-        <p class="mt-2 text-4xl font-semibold text-start px-4  lg:px-20   text-gray-50  sm:tracking-tight lg:text-6xl">
+        <p class="mt-2 text-4xl font-semibold text-end px-4  lg:text-start lg:px-20   text-gray-50  sm:tracking-tight lg:text-6xl">
           Manera De
         </p>
-        <p class="mt-2 text-4xl font-semibold text-start px-4  lg:px-20  text-gray-50  sm:tracking-tight lg:text-6xl">
+        <p class="mt-2 text-4xl font-semibold text-end px-4  lg:text-start lg:px-20  text-gray-50  sm:tracking-tight lg:text-6xl">
           Compartir.
         </p>
-        <div className="px-5 mt-5 lg:px-36  lg:mt-6 flex">
+        <div className="px-9 mt-2 lg:px-36 justify-end lg:justify-start  lg:mt-6 flex">
           <button href="#_" class="relative px-1 lg:px-4 lg:py-4 py-2 font-medium text-white group">
             <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-gray-400 group-hover:bg-gray-800 group-hover:skew-x-12"></span>
             <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-gray-700 group-hover:bg-gray-500 group-hover:-skew-x-12"></span>
@@ -25,7 +25,7 @@ const Content = () => {
         </div>
       </div>
       <div></div>
-      <div className="h-96 flex"> </div>
+      <div className="h-52 lg:96 flex"> </div>
     </section>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,17 +5,17 @@ const Footer = () => {
     <footer className="text-gray-600 body-font  bg-[#ececec]">
       <div className="border-t border-gray-200 bg-[#2d2d2d]">
         <div className="container  py-32 flex flex-wrap justify-center space-x-4 mx-auto items-center">
-          <label for="email" class="leading-7 text-2xl text-gray-300">
+          <label for="email" class="leading-7  text-2xl text-gray-300">
             Subscribete!
           </label>
           <input
             type="email"
             id="email"
             name="email"
-            class="w-72 bg-gray-100 bg-opacity-50  border-yellow-300 focus:border-red-500 focus:bg-transparent focus:ring-2 focus:ring-yellow-700 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            class="w-72 bg-gray-100 bg-opacity-50 mt-2 border-yellow-300 focus:border-red-500 focus:bg-transparent focus:ring-2 focus:ring-yellow-700 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
           />
           <div className="flex md:flex-nowrap flex-wrap justify-center items-end md:justify-center">
-            <button class="text-white bg-yellow-700 py-2 px-8 focus:outline-none hover:bg-gray-600  text-lg">
+            <button class="text-white bg-yellow-700 py-2  mt-2 px-8 focus:outline-none hover:bg-gray-600  text-lg">
               Button
             </button>
           </div>
@@ -46,7 +46,7 @@ const Footer = () => {
       </div>
 
       <div className="bg-[#e3e3e3]">
-        <div className="container mx-auto py-5 px-2 justify-center flex flex-wrap flex-col sm:flex-row">
+        <div className="container mx-auto py-6 px-2 justify-center flex flex-wrap flex-col sm:flex-row">
           <p className="text-gray-600 text-md text-center sm:text-left">
             ©2020 derechos reservados iTAP MEXICO
           </p>
