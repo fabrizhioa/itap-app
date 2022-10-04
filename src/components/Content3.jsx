62640352158e53ac4c59cdbebbe4f4d592dcd843
LouMogollonDev <91619676+luisomogollon@users.noreply.github.com>
2022-10-04 10:02:15 -0400

    creando boton de chilis

--- src/components/Content3.jsx
+++ src/components/Content3.jsx
@@ -3,10 +3,17 @@ import React from "react";
 const Content3 = () => {
   return (
     <section className="text-white bg-hero-pepper py-2 lg:p-44 bg-cover bg-center  ">
-    <div className="h-96 flex">
-      
-    </div>
-  </section>
+      <div className="h-96 flex"></div>
+      <div className="justify-start">
+        
+      </div>
+      <a href="#_" class="relative px-6 py-3 font-bold text-white rounded-lg group">
+<span class="absolute inset-0 w-full h-full transition duration-300 transform -translate-x-1 -translate-y-1 bg-sky-300 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0"></span>
+<span class="absolute inset-0 w-full h-full transition duration-300 transform translate-x-1 translate-y-1 bg-orange-300 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0 mix-blend-screen"></span>
+<span class="relative">Button Text</span>
+</a>
+    </section>
+    
   );
 };
 
