6c052f130101c0098951e6a7317fe3a28abaf43e
LouMogollonDev <91619676+luisomogollon@users.noreply.github.com>
2022-10-05 20:18:42 -0400

    cambio

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,19 +3,19 @@ import React from "react";
 
 const Content = () => {
   return (
-    <section className="text-white bg-hero-max  bg-cover bg-center lg:bg-center  ">
-      <div class="max-w-full  py-1 lg:py-10 lg:px-14  ">
-        <p class="mt-2 text-sx font-semibold text-end lg:text-start px-4 lg:px-20   text-gray-50  sm:tracking-tight lg:text-6xl">
+    <section className="text-white bg-hero-max  bg-cover bg-center  ">
+      <div class="max-w-full mx-auto py-1 lg:py-10 lg:px-32  ">
+        <p class="mt-2 text-sx font-semibold lg:text-start text-end   px-4 lg:px-20   text-gray-50  sm:tracking-tight lg:text-6xl">
           Una Nueva
         </p>
-        <p class="mt-2 text-sx font-semibold text-end px-4  lg:text-start lg:px-20   text-gray-50  sm:tracking-tight lg:text-6xl">
+        <p class="mt-2 text-sx font-semibold lg:text-start text-end  px-4  lg:px-20   text-gray-50  sm:tracking-tight lg:text-6xl">
           Manera De
         </p>
-        <p class="mt-2 text-sx font-semibold text-end px-4  lg:text-start lg:px-20  text-gray-50  sm:tracking-tight lg:text-6xl">
+        <p class="mt-2 text-sx font-semibold lg:text-start text-end px-4  lg:px-20  text-gray-50  sm:tracking-tight lg:text-6xl">
           Compartir.
         </p>
-        <div className="px-9 mt-2 lg:px-36 justify-end lg:justify-start  lg:mt-6 flex">
-          <button href="#_" class="relative px-1 lg:px-4 lg:py-4 py-2 font-medium text-white group">
+        <div className="px-1 mt-2 lg:px-36 lg:justify-start justify-end lg:mt-6 flex">
+          <button href="#_" class="relative px-0 lg:px-4 lg:py-4 py-1 font-medium text-white group">
             <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-gray-400 group-hover:bg-gray-800 group-hover:skew-x-12"></span>
             <span class="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-gray-700 group-hover:bg-gray-500 group-hover:-skew-x-12"></span>
             <span class="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-gray-400 -rotate-12"></span>
@@ -25,7 +25,7 @@ const Content = () => {
         </div>
       </div>
       <div></div>
-      <div className="h-52 lg:96 flex"> </div>
+      <div className="h-96 flex"> </div>
     </section>
   );
 };
